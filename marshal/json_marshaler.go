7fb2dda4c5784a7349c46d7780319ca5b57ebbd8
Jason Parraga <sovietaced@gmail.com>
2023-12-21 12:19:44 -0800

    Document marshal interface and implementations

--- marshal/json_marshaler.go
+++ marshal/json_marshaler.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 )
 
+// JsonMarshaler is an implementation of Marshaler that uses JSON.
 type JsonMarshaler[T any] struct {
 }
 
+// Marshal marshals a go struct into a JSON string.
 func (jm *JsonMarshaler[T]) Marshal(ctx context.Context, value T) (string, error) {
 	bytes, err := json.Marshal(value)
 	if err != nil {
@@ -17,6 +19,7 @@ func (jm *JsonMarshaler[T]) Marshal(ctx context.Context, value T) (string, error
 	return string(bytes), nil
 }
 
+// Unmarshal unmarshals a go struct from a JSON string into a go struct.
 func (jm *JsonMarshaler[T]) Unmarshal(ctx context.Context, valueString string, value *T) error {
 	return json.Unmarshal([]byte(valueString), value)
 }
